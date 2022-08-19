fe6ec823a09707b02cd697c8a18b7de3eb399508
LordOlumide <olumideolubosede@yahoo.com>
2022-08-19 20:14:42 +0100

    Replacing streamSubscription with BlocListener

--- lib/logic/cubits/counter_cubit.dart
+++ lib/logic/cubits/counter_cubit.dart
@@ -1,32 +1,11 @@
 import 'package:bloc/bloc.dart';
-import 'package:bloc_core_concepts/constants/enums.dart';
-import 'dart:async';
-import 'internet_cubit.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
 part 'counter_state.dart';
 
 class CounterCubit extends Cubit<CounterState> {
-  final InternetCubit internetCubit;
-  late StreamSubscription internetStreamSubscription;
-
-  CounterCubit({required this.internetCubit})
-      : super(CounterState(counterValue: 0)) {
-    monitorInternetCubit();
-  }
-
-  void monitorInternetCubit() {
-    internetStreamSubscription = internetCubit.stream.listen((internetState) {
-      if (internetState is InternetConnected &&
-          internetState.connectionType == ConnectivityType.Wifi) {
-        increment();
-      } else if (internetState is InternetConnected &&
-          internetState.connectionType == ConnectivityType.Mobile) {
-        decrement();
-      }
-    });
-  }
+  CounterCubit() : super(CounterState(counterValue: 0));
 
   void increment() => emit(
       CounterState(counterValue: state.counterValue + 1, wasIncremented: true));
@@ -34,9 +13,4 @@ class CounterCubit extends Cubit<CounterState> {
   void decrement() => emit(CounterState(
       counterValue: state.counterValue - 1, wasIncremented: false));
 
-  @override
-  Future<void> close() {
-    internetStreamSubscription.cancel();
-    return super.close();
-  }
 }
--- test/cubits_tests/counter_cubit_test.dart
+++ test/cubits_tests/counter_cubit_test.dart
@@ -1,16 +1,13 @@
 import 'package:bloc_core_concepts/logic/cubits/counter_cubit.dart';
-import 'package:bloc_core_concepts/logic/cubits/internet_cubit.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:test/test.dart';
 import 'package:bloc_test/bloc_test.dart';
 
 void main() {
   group('Tests for the counter cubit', () {
-    InternetCubit internetCubit = InternetCubit(connectivity: Connectivity());
     late CounterCubit counterCubit;
 
     setUp(() {
-      counterCubit = CounterCubit(internetCubit: internetCubit);
+      counterCubit = CounterCubit();
     });
 
     tearDown(() {
@@ -27,7 +24,7 @@ void main() {
     blocTest<CounterCubit, CounterState>(
       'The cubit should emit a CounterState(counterValue: 1, '
       'wasIncremented: true) when cubit.increment function is called.',
-      build: () => CounterCubit(internetCubit: internetCubit),
+      build: () => CounterCubit(),
       act: (cubit) => cubit.increment(),
       expect: () =>
           <CounterState>[CounterState(counterValue: 1, wasIncremented: true)],
@@ -36,7 +33,7 @@ void main() {
     blocTest<CounterCubit, CounterState>(
       'The cubit should emit a CounterState(counterValue: -1, '
       'wasIncremented: false) when cubit.decrement function is called.',
-      build: () => CounterCubit(internetCubit: internetCubit),
+      build: () => CounterCubit(),
       act: (cubit) => cubit.decrement(),
       expect: () =>
           <CounterState>[CounterState(counterValue: -1, wasIncremented: false)],
